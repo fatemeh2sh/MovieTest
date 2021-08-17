21df006bd05172cca2c3c1687d0be8126f3c3260
fatemeh2sh <fa.sharif.sh@gmail.com>
2021-08-17 17:16:00 +0430

    Add Comment for token

--- app/src/main/java/co/sharif/namava/App.kt
+++ app/src/main/java/co/sharif/namava/App.kt
@@ -6,6 +6,10 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class App : Application() {
 
+    //bayad token ro makhfi kard masalan ba estefade az NDK.
+    //vali baray test man inja gozashtam.
+    //ya ba ravesh hay ramz hash ro zakhire konim.
+
     companion object{
         var tokenAuthorize : String = "5a87b2fa9381992a4afa6c8d9b7c1e0b"
     }
