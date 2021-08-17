b2e44e51783c9060e3caa67d3ba25af92450e7f6
fatemeh2sh <fa.sharif.sh@gmail.com>
2021-08-17 17:32:40 +0430

    Change path Item

--- app/src/main/java/co/sharif/namava/ui/play/PlayActivity.kt
+++ app/src/main/java/co/sharif/namava/ui/play/PlayActivity.kt
@@ -41,18 +41,13 @@ class PlayActivity : AppCompatActivity() {
                 .also { exoPlayer ->
                     viewBinding.videoView.player = exoPlayer
 
-//                    val mediaItem = MediaItem.Builder()
-//                            .setUri("<![CDATA[https://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0]]>")
-//                            .setMimeType(MimeTypes.APPLICATION_MPD)
-//                            .build()
-
                     val mediaItem = MediaItem.Builder()
-                            .setUri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+                           // .setUri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+                            .setUri(uri)
                             .setMimeType(MimeTypes.VIDEO_AV1)
                             .setMimeType(MimeTypes.VIDEO_MP4)
                             .build()
 
-                    //val mediaItem = MediaItem.fromUri("<![CDATA[https://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0]]>")
                     exoPlayer.setMediaItem(mediaItem)
                     exoPlayer.playWhenReady = playWhenReady
                     exoPlayer.seekTo(currentWindow, playbackPosition)
